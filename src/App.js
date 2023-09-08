44c9bd3f1648f8179ea766cc046d14bf60b94107
VishalSingh88 <97169607+VishalSingh88@users.noreply.github.com>
2023-09-08 18:10:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 
 import "./App.css";
 import SearchIcon from "./search.svg";
-const API_URL = "http://www.omdbapi.com?apikey=d364ebf3";
+const API_URL = "https://www.omdbapi.com?apikey=d364ebf3";
 
 const movie1 = {
     "Title": "Spiderman and Grandma",
